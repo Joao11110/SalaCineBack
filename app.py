2afb69c7ad794abf7e135fcefb1862c1916f52bc
Joao11110 <joaopmnm@proton.me>
2025-06-29 19:54:32 -0300

    Root da API configurado

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from Model.Connect import initialize_db
 from View.FilmeView import filme_bp
 from View.SalaView import sala_bp
@@ -10,6 +10,19 @@ app.register_blueprint(filme_bp, url_prefix='/api')
 app.register_blueprint(sala_bp, url_prefix='/api')
 app.register_blueprint(sessao_bp, url_prefix='/api')
 
+@app.route('/')
+def home():
+    return jsonify({
+        "message": "A API do SalaCine esta funcionando!",
+        "endpoints": {
+            "filmes": "/api/filmes",
+            "salas": "/api/salas",
+            "sessoes": "/api/sessoes"
+        },
+        "uso": "Use os endpoints acima para interagir com a API."
+    })
+
 initialize_db()
+
 if __name__ == '__main__':
     app.run(debug=True)
